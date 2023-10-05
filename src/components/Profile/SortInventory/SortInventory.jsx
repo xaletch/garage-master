255bb1be76971be4cda04ecc41efd0e4baf44720
xaletch <kiril.kolesnikov0609@mail.ru>
2023-10-05 17:36:03 +0300

    new sort

--- src/components/Profile/SortInventory/SortInventory.jsx
+++ src/components/Profile/SortInventory/SortInventory.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './SortInventory.scss';
 
+const category = ["Дропы", "Кейсы", "Ежедневные бонусы"];
+const priceList = [
+    {name: "0-9", id: 1},
+    {name: "10-49", id: 2},
+    {name: "50-99", id: 3},
+    {name: "100-999", id: 4},
+    {name: "1000+", id: 5},
+];
+
 export const SortInventory = () => {
+    const [selectCategory, setSelectCategory] = useState(0);
+    const [selectedCategoryPrice, setSelectedCategoryPrice] = useState(2);
   return (
     <div className='SortInventory'>
         <div className='SortInventoryCategory'>
             <ul className='categoryList'>
-                <li className='categoryItem active'>Дропы</li>
-                <li className='categoryItem'>Кейсы</li>
-                <li className='categoryItem'>Ежедневные бонусы</li>
+                {category.map((item, index) => (
+                    <li className={selectCategory === index ? 'categoryItem active' : 'categoryItem'} key={index} onClick={() => setSelectCategory(index)}>{item}</li>
+                ))}
             </ul>
         </div>
         <button className='SellEverything'>Продать все</button>
         <div className='SortInventoryPrice'>
             <ul className='priceList'>
-                <li className='priceItem'>0-9₽</li>
-                <li className='priceItem'>10-49₽</li>
-                <li className='priceItem active'>50-99₽</li>
-                <li className='priceItem'>100-999₽</li>
-                <li className='priceItem'>1000+₽</li>
+                {priceList.map((item, index) => (
+                    <li className={selectedCategoryPrice === index ? 'priceItem active' : 'priceItem'} key={index} onClick={() => setSelectedCategoryPrice(index)}>{item.name}₽</li>
+                ))}
             </ul>
         </div>
     </div>
