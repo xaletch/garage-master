0f0275c5a464a8070a8c331972f3b1e4822d571f
xaletch <kiril.kolesnikov0609@mail.ru>
2024-01-16 19:53:32 +0300

    update

--- src/components/Profile/ProfileBottom/ProfileBottom.jsx
+++ src/components/Profile/ProfileBottom/ProfileBottom.jsx
@@ -30,7 +30,18 @@ export const ProfileBottom = ({ image, id, name, price, rarity, status, setOpenS
         rarity === 'экстраординарного типа' ? 'yellow': 
         rarity === 'Засекреченное' ? 'pink' : ''}`}
       >
-        <div className={`status ${status === 'Продан' || status === 'Вывод в процессе' ? 'sold' : ''}`}  onClick={status === 'Продан' || status === 'Вывод в процессе' ? null : () => handleSaleItem(id)}>{price} ₽ {status !== 'Продан' && <span>Продать</span>}</div>
+        <span className='status'
+          style={{ color: `${
+            rarity === 'Запрещённое' ? '#7257a8' :
+            rarity === 'Промышленное качество' ? '#33687e' :
+            rarity === 'Тайное' ? '#7f3535' :
+            rarity === 'Ширпотреб' ? '#307d58' :
+            rarity === 'Армейское качество' ? '#33687e' :
+            rarity === 'экстраординарного типа' ? '#7c842d': 
+            rarity === 'Засекреченное' ? '#7f357b' : ''}`
+          }}
+        >{status}</span>
+        <div className={`statusSale ${status === 'Продан' || status === 'Вывод в процессе' ? 'sold' : ''}`}  onClick={status === 'Продан' || status === 'Вывод в процессе' ? null : () => handleSaleItem(id)}>{price} ₽ {status !== 'Продан' && <span>Продать</span>}</div>
         <div className='withdraw' style={ status === 'Продан' || status === 'Вывод в процессе' ? {display: 'none'} : {display: 'flex'} } onClick={() => handleWithdrawalItem(id)}>
           <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
             <path d="M6.35028 8.42139L6.35028 0.560501L4.7102 0.560501L4.7102 8.42139L1.18978 4.90097L0.0302427 6.0605L5.53024 11.5605L11.0302 6.0605L9.87071 4.90097L6.35028 8.42139Z" fill="#FFF"/>
